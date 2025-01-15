76aaa493ad43dcc4b8192888cff65ce7acede5b6
shilpakarumanchi <shilpa.karumanchi@gmail.com>
2025-01-15 17:51:23 -0500

    Update analytics.sql

--- analytics.sql
+++ analytics.sql
@@ -77,3 +77,34 @@ SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.websit
 FROM accounts
 JOIN orders
 ON accounts.id = orders.account_id;
+
+SELECT a.name, a.primary_poc, o.occurred_at, w.channel
+FROM accounts a
+JOIN orders o
+ON a.id=o.account_id
+JOIN web_events w
+ON a.id=w.account_id
+WHERE a.name LIKE 'walmart' OR a.name LIKE 'Walmart';
+
+Provide a table that provides the region for each sales_rep along with their associated accounts. 
+  Your final table should include three columns: the region name, 
+  the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+SELECT r.name, s.name, a.name
+FROM region r
+JOIN sales_reps s
+ON r.id=s.region_id
+JOIN accounts a
+ON a.sales_rep_id=s.id
+ORDER BY a.name;
+
+Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order. 
+  Your final table should have 3 columns: region name, account name, and unit price.
+  A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+SELECT r.name region_name, a.name account_name, (o.total_amt_usd/(o.total+0.01)) AS unit_price
+FROM region r
+JOIN sales_reps s
+ON r.id=s.region_id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+JOIN orders o
+ON a.id=o.account_id;
