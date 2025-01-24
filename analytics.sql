fe6b05cc11c0a9ef0f972c15c9730e09093c02be
shilpakarumanchi <shilpa.karumanchi@gmail.com>
2025-01-24 12:32:18 -0500

    Update analytics.sql

--- analytics.sql
+++ analytics.sql
@@ -306,11 +306,32 @@ For each account, determine the average amount of each type of paper they purcha
 
 For each account, determine the average amount spent per order on each paper type. 
   Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
-
+SELECT a.name account_name, AVG(o.standard_amt_usd) avg_std_usd, AVG(o.gloss_amt_usd) avg_gloss_usd, AVG(o.poster_amt_usd) avg_poster_usd
+  FROM accounts a
+  JOIN orders o
+  ON a.id=o.account_id
+  GROUP BY account_name;
 
 Determine the number of times a particular channel was used in the web_events table for each sales rep.
   Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
-
+SELECT s.name, w.channel, COUNT(*) num_channel_occur
+  FROM sales_reps s
+  JOIN accounts a
+  ON a.sales_rep_id = s.id
+  JOIN web_events w
+  ON a.id = w.account_id
+  GROUP BY s.name, w. channel
+  ORDER BY num_channel_occur DESC;
 
 Determine the number of times a particular channel was used in the web_events table for each region. 
   Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+SELECT r.name region, w.channel channel, COUNT(*) num_events
+  FROM sales_reps s
+  JOIN accounts a
+  ON a.sales_rep_id = s.id
+  JOIN web_events w
+  ON a.id = w.account_id
+  JOIN region r
+ON r.id = s.region_id
+  GROUP BY region, channel
+ORDER BY num_events DESC;
