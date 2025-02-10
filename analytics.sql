a816b04c47d5072d78609d8e885866c765a7b0e5
shilpakarumanchi <shilpa.karumanchi@gmail.com>
2025-02-10 15:33:16 -0500

    Update analytics.sql

--- analytics.sql
+++ analytics.sql
@@ -335,3 +335,109 @@ SELECT r.name region, w.channel channel, COUNT(*) num_events
 ON r.id = s.region_id
   GROUP BY region, channel
 ORDER BY num_events DESC;
+  
+  How many of the sales reps have more than 5 accounts that they manage?
+SELECT s.id, s.name, COUNT(*) as num_accounts
+  FROM sales_reps s
+  LEFT JOIN accounts a
+  ON s.id = a.sales_rep_id
+  GROUP BY s.id, s.name
+  HAVING num_accounts > 5
+  ORDER BY num_accounts;  
+
+How many accounts have more than 20 orders?
+SELECT account_id, COUNT(*) as num_orders
+  FROM orders
+  GROUP BY account_id
+  HAVING COUNT(*) >20
+  ORDER BY num_orders desc;
+  
+
+Which account has the most orders?
+SELECT account_id, COUNT(*) as num_orders
+  FROM orders
+  GROUP BY account_id
+   ORDER BY num_orders desc
+  LIMIT 1;
+
+Which accounts spent more than 30,000 usd total across all orders?
+  SELECT a.id, a.name, sum(o.total_amt_usd) as total_usd
+  FROM accounts a
+  JOIN orders o
+  ON a.id = o.account_id
+  GROUP BY a.id, a.name
+  HAVING sum(o.total_amt_usd)>30000
+  ORDER BY total_usd;
+  
+Which accounts spent less than 1,000 usd total across all orders?
+HAVING sum(o.total_amt_usd) < 1000
+  ORDER BY total_usd;
+
+Which account has spent the most with us?
+  ORDER BY total_usd desc
+  LIMIT 1;
+
+Which account has spent the least with us?
+
+Which accounts used facebook as a channel to contact customers more than 6 times?
+SELECT a.id, a.name, channel, COUNT(*) as contact_customers
+  FROM accounts a
+  LEFT JOIN web_events w
+  ON a.id = w.account_id
+   HAVING COUNT(*) > 6 AND channel ='facebook'
+  GROUP BY a.id, a.name, channel
+  ORDER BY COUNT(*);  
+
+Which account used facebook most as a channel?
+  WHERE channel = 'facebook'
+ GROUP BY a.id, a.name, channel
+  ORDER BY COUNT(*) desc
+  LIMIT 1;
+
+Which channel was most frequently used by most accounts?
+   GROUP BY a.id, a.name, channel
+  ORDER BY COUNT(*) desc
+  LIMIT 1;
+  
+  Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?
+SELECT extract(year from occurred_at) as year, sum(total_amt_usd) as total_dollars
+  FROM web_events w
+  LEFT JOIN accounts a
+  ON w.account_id = a.id
+  LEFT JOIN order o
+  ON a.id = o.account_id
+  GROUP BY 1
+  ORDER BY 2 desc;
+
+Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?
+SELECT extract(month from occurred_at) as month, sum(total_amt_usd) as total_dollars
+  FROM web_events w
+  LEFT JOIN accounts a
+  ON w.account_id = a.id
+  LEFT JOIN order o
+  ON a.id = o.account_id
+  GROUP BY 1
+  ORDER BY 2 desc;
+
+Which year did Parch & Posey have the greatest sales in terms of total number of orders? Are all years evenly represented by the dataset?
+COUNT(id) as num_orders
+
+Which month did Parch & Posey have the greatest sales in terms of total number of orders? Are all months evenly represented by the dataset?
+
+
+In which month of which year did Walmart spend the most on gloss paper in terms of dollars?
+
+Write a query to display for each order, the account ID, total amount of the order, and the level of the order - ‘Large’ or ’Small’ - depending on if the order is $3000 or more, or smaller than $3000.
+
+Write a query to display the number of orders in each of three categories, based on the total number of items in each order. The three categories are: 'At Least 2000', 'Between 1000 and 2000' and 'Less than 1000'.
+
+We would like to understand 3 different levels of customers based on the amount associated with their purchases. The top level includes anyone with a Lifetime Value (total sales of all orders) greater than 200,000 usd. The second level is between 200,000 and 100,000 usd. The lowest level is anyone under 100,000 usd. Provide a table that includes the level associated with each account. You should provide the account name, the total sales of all orders for the customer, and the level. Order with the top spending customers listed first.
+
+
+We would now like to perform a similar calculation to the first, but we want to obtain the total amount spent by customers only in 2016 and 2017. Keep the same levels as in the previous question. Order with the top spending customers listed first.
+
+
+We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders. Create a table with the sales rep name, the total number of orders, and a column with top or not depending on if they have more than 200 orders. Place the top sales people first in your final table.
+
+
+The previous didn't account for the middle, nor the dollar amount associated with the sales. Management decides they want to see these characteristics represented as well. We would like to identify top performing sales reps, which are sales reps associated with more than 200 orders or more than 750000 in total sales. The middle group has any rep with more than 150 orders or 500000 in sales. Create a table with the sales rep name, the total number of orders, total sales across all orders, and a column with top, middle, or low depending on this criteria. Place the top sales people based on dollar amount of sales first in your final table. You might see a few upset sales people by this criteria!
